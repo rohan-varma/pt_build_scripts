04758422ed8982a29dcba5d9c4be409f8b647fb9
Rohan Varma <rvarm1@fb.com>
2022-06-06 04:34:59 +0000

    update

--- build_pytorch_clean_gpu.sh
+++ build_pytorch_clean_gpu.sh
@@ -1,4 +1,8 @@
-git clean -xfd && git submodule foreach git clean -xf
-git submodule sync ; git submodule update --init --recursive
+#!/bin/bash
+#git clean -xfd && git submodule foreach git clean -xf
+#git submodule sync ; git submodule update --init --recursive
 python setup.py clean
-time USE_XNNPACK=0 USE_MPI=0 REL_WITH_DEB_INFO=1 DEBUG=1 USE_DISTRIBUTED=1 USE_MKLDNN=0 USE_CUDA=1 USE_FBGEMM=0 USE_NNPACK=0 USE_QNNPACK=0 python setup.py develop
+export CMAKE_C_COMPILER_LAUNCHER=ccache
+export CMAKE_CXX_COMPILER_LAUNCHER=ccache
+export CMAKE_CUDA_COMPILER_LAUNCHER=ccache
+time USE_EXPERIMENTAL_CUDNN_V8_API=0 BUILD_CAFFE2=0 BUILD_CAFFE2_OPS=0 USE_NCCL=1 USE_XNNPACK=0 USE_MPI=0 REL_WITH_DEB_INFO=1 DEBUG=1 USE_DISTRIBUTED=1 USE_MKLDNN=0 USE_CUDA=1 USE_FBGEMM=0 USE_NNPACK=0 USE_QNNPACK=0 python setup.py develop
